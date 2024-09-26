43492cb08dac824b7726783a9862d4688ded30c2
ievaprev <56304202+ievaprev@users.noreply.github.com>
2024-09-26 16:52:53 +0300

    Update Stud.cpp

--- Stud.cpp
+++ Stud.cpp
@@ -17,11 +17,18 @@ void ived(Stud &Lok)
 	
 }
 
-void output(Stud &Lok)
+void outputMean(Stud &Lok)
 {
 	cout << setw(15) << left << Lok.vardas << setw(15) << left << Lok.pavarde 
-	<< setw(3) << right << fixed << setprecision(2) << Lok.rez << endl;
+		<< setw(3) << right << fixed << setprecision(2) << Lok.vid << endl;
 }
+
+void outputMedian(Stud& Lok)
+{
+	cout << setw(15) << left << Lok.vardas << setw(15) << left << Lok.pavarde
+		<< setw(3) << right << fixed << setprecision(2) << Lok.med<< endl;
+}
+	
 
 void val(Stud &Lok)
 {
@@ -32,23 +39,35 @@ void val(Stud &Lok)
 
 void finalgrade(Stud &Lok)
 {
-	int elements = Lok.ND.size();
+	Lok.med = 0.4 * median(Lok.ND) + 0.6 * Lok.egz;
+	Lok.vid = 0.4 * mean(Lok.ND) + 0.6 * Lok.egz;
+}
+
+double median(const vector <int>& ND)
+{
+	double med;
+	vector<int> sortedND = ND;
+	sort(sortedND.begin(), sortedND.end());
+	size_t elements = ND.size();
+	if (elements % 2 == 0) {
+		med = (ND[elements / 2 - 1] + ND[elements / 2]) / 2.0;
+	}
+	else {
+		med = ND[elements / 2];
+	}
+	return med;
+}
+
+double mean(const vector <int>& ND)
+{
+	double vidurkis;
+	size_t elements = ND.size();
 	int sum = 0;
 	for (int i = 0; i < elements; i++)
 	{
-		sum = sum + Lok.ND[i];
+		sum = sum + ND[i];
 	}
-	Lok.vid = double(sum / elements);
-	Lok.rez= 0.4 * Lok.vid + 0.6 * Lok.egz;
-}
+	vidurkis = double(sum) / elements;
 
-void median(Stud &Lok)
-{
-	int elements = Lok.ND.size();
-	if (elements % 2 == 0) {
-		Lok.med = (Lok.ND[elements - 1] + Lok.ND[elements + 1]) / 2;
-	}
-	else {
-		Lok.med = Lok.ND[elements / 2];
-	}
+	return vidurkis;
 }
