80a411918a25edf8a67a936fd89c13fa839fbb5f
ievaprev <56304202+ievaprev@users.noreply.github.com>
2024-09-26 16:53:16 +0300

    Update Stud.h

--- Stud.h
+++ Stud.h
@@ -1,13 +1,15 @@
 #include "Mylib.h"
 
 struct Stud {
-	std::string vardas, pavarde;
-	std::vector<double>ND;
-	double vid, med, egz, rez;
+	string vardas, pavarde;
+	vector<int>ND;
+	double vid, med, egz;
 };
 
 void ived(Stud &Lok);
-void output(Stud& Lok);
+void outputMean(Stud& Lok);
+void outputMedian(Stud& Lok);
 void val(Stud &Lok);
 void finalgrade(Stud &Lok);
-void median(Stud& Lok);
+double median(const vector <int>& ND);
+double mean(const vector <int>& ND);
