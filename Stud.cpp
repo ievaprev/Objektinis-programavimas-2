4de3ea3b8f7f75ca3b448c7f760ea402449feedf
ievaprev <56304202+ievaprev@users.noreply.github.com>
2024-09-26 05:45:36 +0300

    Update Stud.cpp

--- Stud.cpp
+++ Stud.cpp
@@ -2,24 +2,25 @@
 
 void ived(Stud &Lok) 
 {
-	//cout << "Input Student Name, Surname and egzam points: ";
 	cin >> Lok.vardas >> Lok.pavarde;
 	cout << "How many homework greades will you enter? " << endl;
 	int k, grade;
 	cin >> k;
-	cout << "Enter homework grades and exam grade at the end:" << endl;
+	cout << "Enter homework grades:" << endl;
 	for (int i = 0; i < k; i++)
 	{
 		cin >> grade;
 		Lok.ND.push_back(grade);
 	}
+	cout << "Enter exam points: ";
 	cin >> Lok.egz;
 	
 }
 
 void output(Stud &Lok)
 {
-	cout << setw(15) << left << Lok.vardas << setw(15) << left << Lok.pavarde << setw(3) << right << Lok.egz << endl;
+	cout << setw(15) << left << Lok.vardas << setw(15) << left << Lok.pavarde 
+	<< setw(3) << right << fixed << setprecision(2) << Lok.rez << endl;
 }
 
 void val(Stud &Lok)
@@ -27,4 +28,27 @@ void val(Stud &Lok)
 	Lok.vardas.clear();
 	Lok.pavarde.clear();
 	Lok.ND.clear();
+}
+
+void finalgrade(Stud &Lok)
+{
+	int elements = Lok.ND.size();
+	int sum = 0;
+	for (int i = 0; i < elements; i++)
+	{
+		sum = sum + Lok.ND[i];
+	}
+	Lok.vid = double(sum / elements);
+	Lok.rez= 0.4 * Lok.vid + 0.6 * Lok.egz;
+}
+
+void median(Stud &Lok)
+{
+	int elements = Lok.ND.size();
+	if (elements % 2 == 0) {
+		Lok.med = (Lok.ND[elements - 1] + Lok.ND[elements + 1]) / 2;
+	}
+	else {
+		Lok.med = Lok.ND[elements / 2];
+	}
 }
