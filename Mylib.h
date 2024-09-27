5cfaa36456dab1744e6a5ef7476680b536f8f2ca
ievaprev <56304202+ievaprev@users.noreply.github.com>
2024-09-27 05:59:22 +0300

    Update Mylib.h

--- Mylib.h
+++ Mylib.h
@@ -2,6 +2,12 @@
 #include<iomanip>
 #include<string>
 #include<vector>
+#include<algorithm>
+#include<limits> 
+#include<ios>
+#include<cstdlib>
+#include<fstream>
+#include<sstream> 
 
 using std::endl;
 using std::cout;
@@ -11,5 +17,12 @@ using std::right;
 using std::setw; //isvedamo zodzio plotis
 using std::setprecision;
 using std::fixed;
+using std::getline;
+using std::streamsize;
+using std::ifstream;
+using std::numeric_limits;
 using std::string;
 using std::vector;
+using std::runtime_error;
+using std::istringstream;
+using std::cerr;
