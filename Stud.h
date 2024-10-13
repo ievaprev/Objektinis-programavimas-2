37ac13aef0a23cd6abfb2a9de029086e41489e16
ievaprev <56304202+ievaprev@users.noreply.github.com>
2024-10-13 17:33:37 +0300

    Update Stud.h

--- Stud.h
+++ Stud.h
@@ -6,12 +6,13 @@ struct Stud {
 	double vid, med, egz;
 };
 
-void ived(Stud &Lok);
+void ived(Stud& Lok);
 void outputMean(Stud& Lok);
 void outputMedian(Stud& Lok);
-void val(Stud &Lok);
-void finalgrade(Stud &Lok);
+void val(Stud& Lok);
+void finalgrade(Stud& Lok);
 void readFile(const string& fileName, vector <Stud>& stud);
 void outputFile(vector<Stud>& stud);
 double median(const vector <int>& ND);
 double mean(const vector <int>& ND);
+void generateFile(const string& fileName, const int& number); 
