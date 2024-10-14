6cd7aeec43ca5e9eab36f3f05e8a41d293b6538a
ievaprev <56304202+ievaprev@users.noreply.github.com>
2024-10-14 21:18:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "Mylib.h"
 #include "Stud.h"
+#include "Timer.h"
 
 int main()
 {
     vector<Stud> Vec1;
+    vector<Stud> smart, dumb; 
     Stud Temp;
     cout << "Ar norite nuskaityti informacija is tekstinio failo? (0 - taip, 1 - ne)" << endl;
     int textAts;
@@ -25,6 +27,7 @@ int main()
         }
         catch (const exception& e) {
             cerr << e.what() << endl;  
+  
         }
     }
 
@@ -58,9 +61,44 @@ int main()
                 generateFile("studentai" + to_string(b) + ".txt", b);
                 readFile("studentai" + to_string(b) + ".txt", Vec1);
 
+                Timer t;
+                for (Stud& Temp : Vec1) {
+                    grouping(smart, dumb, Temp);
+                }
+                cout << "Studentu grupavimas uztruko: " << t.elapsed() << " s\n";
+
+                cout << "    " << endl;
+
+                cout << "Pasirinkite pagal ka noretumete rusiuoti sugrupuotus studentus:\n1 - varda, 2 - pavarde, 3 - galutini rezultata maz., 4 - galutini rezultata did." << endl;
+                int ats;
+                while (true) {
+                    try {
+                        if (!(cin >> ats)) {
+                            cin.clear();
+                            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                            throw invalid_argument("Klaida: prasome ivesti skaiciu, o ne raide.");
+                        }
+
+                        if (ats != 1 && ats != 2 && ats != 3 && ats != 4) {
+                            throw runtime_error("Klaida: prasome pasirinkti viena is 4 variantu.");
+                        }
+                        break;
+                    }
+                    catch (const exception& e) {
+                        cerr << e.what() << endl;
+                    }
+                }
+
+                sort(smart, dumb, ats);
+                cout << "   " << endl;
+
+                outputInFile("Smart studentai" + to_string(b) + ".txt", smart);
+                outputInFile("Dumb studentai" + to_string(b) + ".txt", dumb);
+
                 b *= 10;
                 system("pause");
             }
+            
         }
         else {
             cout << "Pateikite failo varda: " << endl;
@@ -114,9 +152,11 @@ int main()
                     cin.ignore(numeric_limits<streamsize>::max(), '\n');
                     throw invalid_argument("Klaida: prasome ivesti skaiciu, o ne raide.");
                 }
+
                 if (choice != 0 && choice != 1) {
                     throw runtime_error("Klaida: prasome pasirinkti 0 - vidurkiu arba 1 - mediana.");
                 }
+
                 break;
             }
             catch (const exception& e) {
