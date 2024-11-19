a13a8aedc52d49d0899536dbebd625e3c5ef4959
Ieva Prevelyte <56304202+ievaprev@users.noreply.github.com>
2024-11-19 10:47:14 +0200

    Update Files.cpp

--- List/src/Files.cpp
+++ List/src/Files.cpp
@@ -1,54 +1,34 @@
 #include "Stud.h"
 #include "Timer.h"
 
-void readFile(const string& fileName, list <Stud>& stud) {
+void readFile(const string& fileName, list<Stud>& stud) {
 
 	Timer r;
 	ifstream inFile(fileName);
-
 
 	try {
 		if (!inFile.is_open()) {
 			throw runtime_error("Error: unable to open file: " + fileName);
 		}
+
 		string line;
 		getline(inFile, line);
+
 		while (getline(inFile, line)) {
 			istringstream iss(line);
-			Stud Lok1;
-			int score;
-
-			iss >> Lok1.pavarde >> Lok1.vardas;
-
-			Lok1.ND.clear();
-			while (iss >> score) {
-				Lok1.ND.push_back(score);
-			}
-
-			if (Lok1.ND.empty()) {
-				cerr << "Error: No ND scores found in line " << endl;
-				continue;
-			}
-			for (int i = 0; i < Lok1.ND.size(); i++) {
-				if (Lok1.ND.at(i) < 1 || Lok1.ND.at(i) > 10) {
-					throw runtime_error("Error: ND score must be between 1 and 10");
-				}
-			}
-
-			Lok1.egz = Lok1.ND.back();
-			Lok1.ND.pop_back();
+			Stud Lok1(iss);
 
 			stud.push_back(Lok1);
-
 		}
+
 		inFile.close();
 	}
-	catch (exception& e) {
+	catch (const exception& e) {
 		cerr << e.what() << endl;
 		exit(EXIT_FAILURE);
 	}
 
-	cout << "Failo " << fileName <<" nuskaitymas uztruko : " << r.elapsed() << " s\n";
+	cout << "Failo " << fileName << " nuskaitymas uztruko : " << r.elapsed() << " s\n";
 }
 
 
@@ -102,7 +82,7 @@ void outputFile(list<Stud>& stud)
 
 	for (Stud& duom : stud)
 	{
-		cout << setw(18) << left << duom.pavarde << setw(15) << left << duom.vardas
+		cout << setw(18) << left << duom.getLastName() << setw(15) << left << duom.getName()
 			<< setw(3) << right << fixed << setprecision(2) << duom.vid << "                "
 			<< setw(3) << right << fixed << setprecision(2) << duom.med << endl;
 	}
@@ -120,7 +100,7 @@ void outputInFile(const string& fileName, list<Stud>& stud)
 
 	for (Stud& duom : stud)
 	{
-		input << setw(18) << left << duom.pavarde << setw(15) << left << duom.vardas
+		input << setw(18) << left << duom.getLastName() << setw(15) << left << duom.getName()
 			<< setw(3) << right << fixed << setprecision(2) << duom.vid << endl;
 	}
 
