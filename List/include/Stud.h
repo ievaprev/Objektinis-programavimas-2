aa578ce304c8b83850384d1a3eec8872feb330d5
Ieva Prevelyte <56304202+ievaprev@users.noreply.github.com>
2024-11-28 16:45:33 +0200

    Update Stud.h

--- List/include/Stud.h
+++ List/include/Stud.h
@@ -7,8 +7,14 @@ public:
 	vector<int>ND;
 	double vid, med, egz;
 
-	Stud() : egz(0) { }
-	Stud(std::istream& is);
+	Stud() : vardas(""), pavarde(""), vid(0), med(0), egz(0) {}
+	Stud(string vardas, string pavarde, vector<int> nd, int egz) : 
+		vardas(vardas), pavarde(pavarde), ND(nd), egz(egz), vid(0), med(0) {}
+
+	Stud(std::istream& is) : Stud() { 
+		is >> *this;
+	}
+
 	void setName(string tempVardas) {
 		vardas = tempVardas;
 	}
@@ -17,20 +23,49 @@ public:
 		pavarde = tempPavarde;
 	}
 	inline string getLastName() const { return pavarde; }
-	std::istream& readStudent(std::istream&);
 
-	~Stud() = default;
+	//operatoriai
+	friend std::istream& operator>>(std::istream& in, Stud& student);
+	friend std::ostream& operator<<(std::ostream& out, const Stud& student);
+
+	//metodu perdengimas
+	void input(Stud& Lok);
+	void input(const string& fileName, const int& number);
+	void input(const string& fileName, list <Stud>& stud);
+	void output(list<Stud>& stud, int choice);
+	void output(list<Stud>& stud);
+	void output(const string& fileName, list<Stud>& stud);
+
+	~Stud() {}//I. destruktorius
+
+	//II. copy constructor
+	Stud(const Stud& copy) {
+		vardas = copy.vardas;
+		pavarde = copy.pavarde;
+		ND = copy.ND; 
+		vid = copy.vid;
+		med = copy.med;
+		egz = copy.egz;
+	}
+
+	///III. copy ssigment
+	Stud& operator=(const Stud& other) {
+		if (this != &other) {
+			vardas = other.vardas;
+			pavarde = other.pavarde;
+			ND = other.ND;
+			vid = other.vid;
+			med = other.med;
+			egz = other.egz;
+		}
+		return *this;
+	}
 };
 
-void ived(Stud& Lok);
-void outputMean(list<Stud>& stud);
-void outputMedian(list<Stud>& stud);
 void val(Stud& Lok);
 void finalgrade(Stud& Lok);
-void readFile(const string& fileName, list <Stud>& stud);
 double median(const vector <int>& ND);
 double mean(const vector <int>& ND);
-void generateFile(const string& fileName, const int& number); 
 void grouping1(list<Stud>& smart, list<Stud>& dumb, list<Stud>& list);
 void grouping2(list<Stud>& smart, list<Stud>& list);
 void grouping3(list<Stud>& smart, list<Stud>& list);
@@ -39,5 +74,4 @@ bool sortBySurname(Stud& a, Stud& b);
 bool sortDecending(Stud& a, Stud& b);
 bool sortAscending(Stud& a, Stud& b);
 void sortByChoice(list<Stud>& stud, const int& choice);
-void outputFile(list<Stud>& stud);
-void outputInFile(const string& fileName, list<Stud>& stud);
+void demonstration();
