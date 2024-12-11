a3fd369d08a453b147803867a2c1f1463dd05062
Ieva Prevelyte <56304202+ievaprev@users.noreply.github.com>
2024-12-12 00:09:11 +0200

    Update run.bat

--- List/run.bat
+++ List/run.bat
@@ -1,10 +1,21 @@
+@echo off
+
 cd List
 mkdir build
 cd build
-cmake ..
+
+cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
+
 cd ..
-copy src\*.txt build
-cd build\Release
+copy src\*.txt build\Release
+
+cd build
+ctest -C Release --output-on-failure
+
+cd Release
+ProjectTest.exe
+pause
+
 List.exe
 pause
